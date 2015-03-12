7dc284816676961da1d4140c677ebddc9a8a50f1
Francis Baster <francisbaster@gmail.com>
2015-03-12 18:05:49 +1300

    Remove old modules from ServerProxy

--- src/main/java/com/the_beast_unleashed/flameprotect/server/ServerProxy.java
+++ src/main/java/com/the_beast_unleashed/flameprotect/server/ServerProxy.java
@@ -8,7 +8,6 @@ package com.the_beast_unleashed.flameprotect.server;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 
 import com.the_beast_unleashed.flameprotect.FlameProtectLogger;
@@ -16,9 +15,6 @@ import com.the_beast_unleashed.flameprotect.server.ServerConfigHandler;
 import com.the_beast_unleashed.flameprotect.server.log.EventLogger;
 import com.the_beast_unleashed.flameprotect.server.log.LogCommand;
 import com.the_beast_unleashed.flameprotect.server.log.LogEventHandler;
-import com.the_beast_unleashed.flameprotect.server.protect.ProtectCommand;
-import com.the_beast_unleashed.flameprotect.server.protect.ProtectEventHandler;
-import com.the_beast_unleashed.flameprotect.server.protect.ProtectTickHandler;
 
 import java.util.logging.Level;
 
@@ -39,13 +35,10 @@ public class ServerProxy {
 
     public void postInit(FMLPostInitializationEvent event) {    
         EventLogger.log(Level.INFO, "registering EventHandlers");
-        MinecraftForge.EVENT_BUS.register(new ProtectEventHandler());
         MinecraftForge.EVENT_BUS.register(new LogEventHandler());
-        TickRegistry.registerScheduledTickHandler(new ProtectTickHandler(), Side.SERVER);
     }
 
     public void serverLoad(FMLServerStartingEvent event) {
-        event.registerServerCommand(new ProtectCommand());
         event.registerServerCommand(new LogCommand());
         event.registerServerCommand(new WhatCommand());
     }
